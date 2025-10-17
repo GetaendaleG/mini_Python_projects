05befc80e0117f033fdd7706c424ad55d1efd02f
Getaendale_G <GetaendaleG@gmial.com>
2025-10-17 21:36:36 +0300

    add the quetions and evaluator

--- quiz_game.py
+++ quiz_game.py
@@ -1,9 +1,40 @@
 print('Welcome to my computer Quiz')
 
-playing = input("Do you want to play: ")
+playing = input("Do you want to play: ").lower()
 
 if playing != "yes":
     print("Bye :(")
 else :
     print("Let's play :)")
 
+score = 0
+
+answer = input("What does cpu stands for?: ").lower()
+if answer == "central processing unit":
+    print("correct")
+    score +=1
+else:
+    print('wrong')
+
+answer = input("What does ram stands for?: ").lower()
+if answer == "random access memory":
+    print("correct")
+    score +=1
+else:
+    print('wrong')
+
+answer = input("What does gpu stands for?: ").lower()
+if answer == "graphical processing unit":
+    print("correct")
+    score +=1
+else:
+    print('wrong')
+
+answer = input("What does psu stands for?: ").lower()
+if answer == "power supply unit":
+    print("correct")
+    score +=1
+else:
+    print('wrong')
+
+print("your score will be: "+ str(score))
