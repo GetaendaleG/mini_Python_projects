e84f0c6ee767933218f1cd1e712baac678a60342
Getaendale_G <GetaendaleG@gmial.com>
2025-10-20 13:52:10 +0300

    add the main section and done

--- number_guesser.py
+++ number_guesser.py
@@ -1,13 +1,31 @@
 import random
 
-top_of_number = input('Enter the top range: ')
+top_of_range = input('Enter the top range: ')
 
-if  top_of_number.isdigit():
-    top_of_number = int(top_of_number)
-    if top_of_number <= 0:
+if  top_of_range.isdigit():
+    top_of_range = int(top_of_range)
+    if top_of_range <= 0:
         print('Next time enter larger number than zero')
         quit()
 else:
     print('Next time enter a number')
     quit()
-print(top_of_number)
+
+score = 0
+
+while True:
+    random_no = random.randint(0,top_of_range)
+    guessed_no = input("Guess the number (Enter quit if you want to quit): ")
+    if guessed_no == "quit":
+        quit()
+    else:
+        guessed_no = int(guessed_no)
+        if guessed_no == random_no:
+            score += 1
+            print("You have guessed right the number is: "+ str(guessed_no))
+            print("Score = "+ str(score))
+        else:
+            print("Incorrect, The number was : "+ str(random_no))
+            print("Score = "+ str(score))    
+    
+
